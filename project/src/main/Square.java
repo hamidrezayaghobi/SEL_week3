223a0bb9e973ed2f60f8ea0ded89f4bfeb3d76dd
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2023-11-22 18:39:30 +0330

    write the getter for the side attribute of the square class

--- project/src/main/Square.java
+++ project/src/main/Square.java
@@ -2,7 +2,12 @@ package main;
 
 public class Square {
     private int side;
+
     public Square(int initSide) {
         side = initSide;
+    }
+
+    public int getSide() {
+        return 0;
     }
 }
